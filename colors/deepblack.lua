460e26a6450cd66becab574c50c8430c17ebf82d
dzhma <dzhma@proton.me>
2024-07-11 22:58:40 +0400

    Working code update

--- colors/deepblack.lua
+++ colors/deepblack.lua
@@ -1,2 +1,9 @@
--- package.loaded['deepblack'] = nil
-require("deepblack").load()
+vim.cmd("hi clear")
+if vim.fn.exists("syntax_on") then
+    vim.cmd("syntax reset")
+end
+
+vim.o.termguicolors = true
+vim.g.colors_name = "deepblack"
+
+require("deepblack").setup()
